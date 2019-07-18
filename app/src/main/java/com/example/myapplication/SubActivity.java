1ebb0a9af66cc229e47ecda6fae6ad809b164e85
xumingyi <xumingyi@smartens-tek.com>
2019-07-19 00:26:55 +0800

    更新方案三错误写法，全局配置windowIsTranslucent属性导致的页面点击或者双击错误

--- app/src/main/java/com/example/myapplication/SubActivity.java
+++ app/src/main/java/com/example/myapplication/SubActivity.java
@@ -1,18 +1,17 @@
 package com.example.myapplication;
 
 import android.content.Context;
-import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 
-public class MainActivity extends BaseActivity {
+public class SubActivity extends BaseActivity {
 
     private Context mContext;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main2);
         mContext = this;
 
 
@@ -22,15 +21,6 @@ public class MainActivity extends BaseActivity {
                 | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
         getWindow().setStatusBarColor(Color.TRANSPARENT);
         getWindow().setNavigationBarColor(Color.TRANSPARENT);
-
-
-        findViewById(R.id.test_id).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                mContext.startActivity(new Intent(MainActivity.this,
-                        SubActivity.class));
-            }
-        });
 
         //全屏，滑动弹出状态栏和单行栏
 //        if (Build.VERSION.SDK_INT >= 19) {
